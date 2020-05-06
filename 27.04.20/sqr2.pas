bea059c5acee92492af3d507d84e58cb67497cf2
Tim55z <timofey.blinov@gmail.com>
2020-05-06 21:41:11 +0300

    first commit

---
+++ 27.04.20/sqr2.pas
@@ -0,0 +1,26 @@
+﻿program sqr2;
+var 
+x, n: integer;
+a, b: string;
+begin
+  while true do begin
+    write('please enter integer value' + a + #10 + 'N = ');  
+    x := 1;
+    a := '';
+    readln(b);
+    if integer.TryParse(b, n) and (n > 0) then begin
+      while sqr(x) < n do begin
+          print(sqr(x));
+          x := x + 1;
+      end;
+      break;
+    end;  
+    if not integer.TryParse(b, n) then begin
+      write('error: "' + b + '" is not integer' + #10);
+      a := ' again';
+    end  
+    else    
+      write('error: "' + n + '" <= 0' + #10);
+      a := ' again';
+  end  
+end.
